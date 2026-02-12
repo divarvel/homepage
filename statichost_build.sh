879e391943d79ec6a27093b61e3de36ec874d26d
Clement Delafargue <clement@delafargue.name>
2026-02-13 00:08:24 +0100

    fix deployment

---
+++ statichost_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+sudo apk add gmp-dev oniguruma-dev
+opam install dune
+opam env > opam.env
+source opam.env
+opam install . --deps-only -y
+dune build
+_build/install/default/bin/homepage
